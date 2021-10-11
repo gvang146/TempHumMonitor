4a1667577ac5403a04436bea7f69921655a178e8
gvang146 <gvang146@gmail.com>
2021-10-11 18:26:44 -0500

    a

--- Src/MTechNodeAPI/MMTechNodeAPI/Controllers/SensorController.cs
+++ Src/MTechNodeAPI/MMTechNodeAPI/Controllers/SensorController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MMTechNodeAPI.Data;
 using MMTechNodeAPI.Models;
-//PLC programmable logic controller - controlled by PLC
+
 namespace MMTechNodeAPI.Controllers
 {
     [ApiController]
